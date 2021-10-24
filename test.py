7f42d4eddfd7259f2a8a3992a5dc1c4df3cc14f6
technicakidz <y.technicakidz08@gmail.com>
2021-10-25 00:32:56 +0900

    test

---
+++ test.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('タイトル：テスト')
+
+# サイドバー
+# ファイルアップロード
+uploaded_file = st.sidebar.file_uploader("ファイルアップロード", type='csv') 
+
+# グラフx軸設定
+x_axis = st.sidebar.selectbox('x軸項目',('材料A添加量', '材料B添加量', '材料C添加量'))
+
+
+# メイン画面
+st.header('読み込みデータ表示')
+if uploaded_file is not None:
+    # アップロードファイルをメイン画面にデータ表示
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+
+    # 選ばれたx軸の値からグラフ化
+    st.header('グラフ表示')
+    fig = px.scatter(x=df[x_axis], y=df['引張強度'])
+    st.plotly_chart(fig, use_container_width=True)
